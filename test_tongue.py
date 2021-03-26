6f150129c4da165bef3bd1024100008ece74d8e8
ianzih <abc021789@gmail.com>
2021-03-27 01:30:53 +0800

    commit first maskrcnn

---
+++ test_tongue.py
@@ -0,0 +1,152 @@
+from mrcnnn import visualize
+import mrcnnn.model as modellib
+from mrcnnn import utils
+import os
+import sys
+import random
+import math
+import numpy as np
+import skimage.io
+import matplotlib
+import matplotlib.pyplot as plt
+import cv2
+import time
+from mrcnnn.config import Config
+from datetime import datetime
+import time
+# Root directory of the project
+ROOT_DIR = os.getcwd()
+
+sys.path.append(ROOT_DIR)  # To find local version of the library
+
+
+
+# Directory to save logs and trained model
+MODEL_DIR = os.path.join(ROOT_DIR, "logs")
+
+# Local path to trained weights file
+COCO_MODEL_PATH = os.path.join(MODEL_DIR, "mask_rcnn_shapes_0040.h5")
+# Download COCO trained weights from Releases if needed
+if not os.path.exists(COCO_MODEL_PATH):
+    utils.download_trained_weights(COCO_MODEL_PATH)
+    print("************NO***********")
+
+# Directory of images to run detection on
+IMAGE_DIR = os.path.join(ROOT_DIR, "images")
+
+
+class ShapesConfig(Config):
+    """Configuration for training on the toy shapes dataset.
+    Derives from the base Config class and overrides values specific
+    to the toy shapes dataset.
+    """
+    # Give the configuration a recognizable name
+    NAME = "shapes"
+
+    # Train on 1 GPU and 8 images per GPU. We can put multiple images on each
+    # GPU because the images are small. Batch size is 8 (GPUs * images/GPU).
+    GPU_COUNT = 1
+    IMAGES_PER_GPU = 2
+
+    # Number of classes (including background)
+    NUM_CLASSES = 1 + 1  # background + 3 shapes
+
+    # Use small images for faster training. Set the limits of the small side
+    # the large side, and that determines the image shape.
+    IMAGE_MIN_DIM = 320
+    IMAGE_MAX_DIM = 384
+
+    # Use smaller anchors because our image and objects are small
+    RPN_ANCHOR_SCALES = (8 * 6, 16 * 6, 32 * 6, 64 * 6,
+                         128 * 6)  # anchor side in pixels
+
+    # Reduce training ROIs per image because the images are small and have
+    # few objects. Aim to allow ROI sampling to pick 33% positive ROIs.
+    TRAIN_ROIS_PER_IMAGE = 100
+
+    # Use a small epoch since the data is simple
+    STEPS_PER_EPOCH = 100
+
+    # use small validation steps since the epoch is small
+    VALIDATION_STEPS = 50
+
+class InferenceConfig(ShapesConfig):
+    # Set batch size to 1 since we'll be running inference on
+    # one image at a time. Batch size = GPU_COUNT * IMAGES_PER_GPU
+    GPU_COUNT = 1
+    IMAGES_PER_GPU = 1
+
+
+config = InferenceConfig()
+
+model = modellib.MaskRCNN(mode="inference", model_dir=MODEL_DIR, config=config)
+
+
+def color_splash(image, mask):
+    """Apply color splash effect.
+    image: RGB image [height, width, 3]
+    mask: instance segmentation mask [height, width, instance count]
+
+    Returns result image.
+    """
+    # Make a grayscale copy of the image. The grayscale copy still
+    # has 3 RGB channels, though.
+    gray = (0, 0, 0)
+    # Copy color pixels from the original color image where mask is set
+    if mask.shape[-1] > 0:
+        # We're treating all instances as one, so collapse the mask into one layer
+        mask = (np.sum(mask, -1, keepdims=True) >= 1)
+        splash = np.where(mask, image, gray).astype(np.uint8)
+    else:
+        splash = gray.astype(np.uint8)
+    return splash
+
+
+def detect_and_color_splash(model, image_path=None,image_name=None):
+    if image_path:
+        # Run model detection and generate the color splash effect
+        print("Running on {}".format(image_path))
+        # Read image
+        image = skimage.io.imread(image_path)
+        # Detect objects
+        r = model.detect([image], verbose=1)[0]
+        # Color splash
+        flag = r['rois'].shape[0]
+        if not flag:
+            print("\n*** No instances to display *** \n")
+        else:    
+            splash = color_splash(image, r['masks'])
+            # Save output
+            file_name = image_name
+            skimage.io.imsave("./tongue_result_splash/splash_" + file_name, splash)
+            print("Saved to ", file_name )
+
+
+# Create model object in inference mode.
+model = modellib.MaskRCNN(mode="inference", model_dir=MODEL_DIR, config=config)
+
+# Load weights trained on MS-COCO
+model.load_weights(COCO_MODEL_PATH, by_name=True)
+
+# Index of the class in the list is its ID. For example, to get ID of
+# the teddy bear class, use: class_names.index('teddy bear')
+class_names = ['BG', 'tongue']
+# Load a random image from the images folder
+directory_name = "./TestImage"
+
+for file_names in os.listdir(directory_name):
+    print(file_names)
+    image = skimage.io.imread(directory_name + "/" + file_names)
+    start = time.time()
+
+    # Run detection
+    results = model.detect([image], verbose=1)
+    # Visualize results
+    detect_and_color_splash(model, image_path = directory_name + "/" + file_names  , image_name=file_names)
+    
+    #r = results[0]
+    #visualize.display_instances(
+    #    image, r['rois'], r['masks'], r['class_ids'], class_names, r['scores'])
+    end = time.time()
+    print("Time : ", round(end-start, 2))
+    
